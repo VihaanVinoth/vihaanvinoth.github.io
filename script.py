3e6b3e54319418ecee518fe6a5f69f16e24d0dd3
Vihaan Vinoth <vertialX1000@outlook.com.au>
2026-01-08 12:53:25 +1100

    init commit

---
+++ script.py
@@ -0,0 +1,13 @@
+number = 29 
+
+for i in range(2, number): 
+
+    if number % i == 0: 
+
+        print(f"{number} is not a prime number.") 
+
+        break 
+
+else: 
+
+    print(f"{number} is a prime number.") 
