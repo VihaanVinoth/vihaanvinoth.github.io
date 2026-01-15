b85fa022ae9df75070352085a802e83439ab2090
Vihaan Vinoth <vertialX1000@outlook.com.au>
2026-01-15 22:12:41 +1100

    fix

--- post-struct.js
+++ post-struct.js
@@ -74,8 +74,8 @@ for (const file of fs.readdirSync(postsDir)) {
         <title>${title} | Vihaan Vinoth</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="robots" content="index, follow">
-        <link rel="stylesheet" href="/stylesheets/reset.css?v=1.9.2">
-        <link rel="stylesheet" href="/stylesheets/style.css?v=1.9.2">
+        <link rel="stylesheet" href="/stylesheets/reset.css?v=1.9.3">
+        <link rel="stylesheet" href="/stylesheets/style.css?v=1.9.3">
         <meta name="description" content="${summary}">
         <meta property="og:title" content="${title} - Vihaan Vinoth">
         <meta property="og:description" content="${summary}">
